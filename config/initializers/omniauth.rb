cc9bd199aba9ceded7c0493f9108f4f35609c804
William Weckl <william.weckl@gmail.com>
2015-02-10 13:31:31 -0800

    dashboard view name correct, removed facebook path from initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], callback_path: '/auth/facebook/callback'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
